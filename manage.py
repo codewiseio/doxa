418c694160f2606038502485d612b8fb31947b8c
codingcarttechnologies <codingcarttechnologies@gmail.com>
2017-07-13 19:18:58 +0530

    manage.py update

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doxa.settings")
+    os.environ("DJANGO_SETTINGS_MODULE", "doxa.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
